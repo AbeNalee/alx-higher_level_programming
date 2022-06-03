762a24335913f0121205f2706091ca598795652d
Abe <43585616+AbeNalee@users.noreply.github.com>
2022-06-03 15:15:11 +0300

    import modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+def infinite_addition():
+    infinite_sum = 0
+    ac = len(argv)
+
+    for index, args in enumerate(argv):
+        if index == 0:
+            continue
+        infinite_sum += int(args)
+    print("{:d}".format(infinite_sum))
+
+
+if __name__ == "__main__":
+    infinite_addition()
