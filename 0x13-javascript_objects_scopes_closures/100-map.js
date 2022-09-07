28f7427bca12eedda740d617e15df318076295c3
Abe <43585616+AbeNalee@users.noreply.github.com>
2022-09-07 19:32:23 +0300

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-function printList(l) {
-	console.log(l);
-	console.log(l.map((item, index) => item * index));
-}
-printList(list);
+console.log(list.map((item, index) => item * index));
